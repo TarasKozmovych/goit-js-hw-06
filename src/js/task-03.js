97b8703b8eb2acc4695b4c292d6039a557795e04
Taras Kozmovych <taraskom0404@gmail.com>
2022-07-24 15:44:10 +0300

    task-3

--- src/js/task-03.js
+++ src/js/task-03.js
@@ -12,3 +12,13 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+const gallery = document.querySelector(".gallery");
+
+
+const markup = images
+  .map(
+    ({ url, alt }) =>
+      `<li class="list-item"><img src="${url}" alt="${alt}" ></li>`)
+  .join("");
+
+gallery.innerHTML = markup;
