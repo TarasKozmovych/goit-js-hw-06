f909257ce52bf4af80df5d889aa00df9112b4a02
Taras Kozmovych <taraskom0404@gmail.com>
2022-07-24 20:25:19 +0300

    task-9

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,12 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const btn = document.querySelector('.change-color');
+const span = document.querySelector('.color');
+const body = document.querySelector('body');
+
+const changeBodyColor = () => {
+  body.style.backgroundColor = getRandomHexColor();
+  span.textContent = getRandomHexColor();
+};
+btn.addEventListener("click", changeBodyColor);
