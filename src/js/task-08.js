0e3546b7fbdded53d4fd3eec64a2304cd3f420a9
Taras Kozmovych <taraskom0404@gmail.com>
2022-07-24 20:05:30 +0300

    task-8

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,19 @@
+const form = document.querySelector('.login-form');
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    if (
+        event.currentTarget.email.value === "" ||
+        event.currentTarget.password.value === ""
+    ) {
+        alert("Всі поля форми повінні бути заповненими.")
+    }
+    else {
+        const {
+            elements: { email, password },
+        } = event.currentTarget;
+        const userDetails = { email: email.value, Password: password.value };
 
+        console.log(userDetails);
+        event.currentTarget.reset();
+    }
+});
