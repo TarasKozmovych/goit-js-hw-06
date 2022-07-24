e9da6bd6559c540cb1d96117ae2c601ef36f327e
Taras Kozmovych <taraskom0404@gmail.com>
2022-07-24 18:58:34 +0300

    task-6

---
+++ src/js/task-06.js
@@ -0,0 +1,27 @@
+const textInput = document.querySelector("#validation-input");
+const length = +textInput.dataset.length;
+textInput.addEventListener("blur", checkValue);
+
+function checkValue(element) {
+    if (element.currentTarget.value.length === length) {
+        textInput.classList.remove("invalid");
+        textInput.classList.add("valid");
+    } else {
+        textInput.classList.remove("valid");
+        textInput.classList.add("invalid");
+    }
+}
+
+
+// В інтернеті ще на такий розв’язок натикнувся...
+
+
+// document.getElementById("validation-input").onblur = function () {
+//     if (this.getAttribute('data-length') > this.value.length) {
+//         this.classList.remove('valid');
+//         this.classList.add('invalid');
+//     } else {
+//         this.classList.remove('invalid');
+//         this.classList.add('valid');
+//     }
+// };
